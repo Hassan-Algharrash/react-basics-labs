a62ef343e400d7579a0a4527f2e17c2b2662ab85
hassa <20112840@mail.wit.ie>
2026-09-22 12:00:38 +0100

    Add task: form added

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
-
+import AddTaskForm from './components/Form';
 
 
 function App() {
@@ -47,7 +47,7 @@ function App() {
   )
   )
   } 
-
+      <AddTaskForm/>
     </div>
   );
 
