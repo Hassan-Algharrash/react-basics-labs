aaa4863ee7edbcc0524ac7cc581210d6aa88a95b
hassa <20112840@mail.wit.ie>
2026-09-22 13:01:14 +0100

    Exercise Week 2 Complete

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,10 +1,19 @@
 const Task = (props) => {
     return (
-        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+        <div className="card" style={{
+            backgroundColor: 
+            props.done ? 'lightgrey' : '#5bb4c4'
+            }}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
-            <p className="priority">{props.priority} </p>
+            <p className="priority" style={{
+                backgroundColor:
+                props.done ? "lightgray" :
+                props.priority === 'high' ? '#FF0000':
+            props.priority === 'medium' ? '#FFEA00':
+            props.priority === 'low' ? '#00FF00': 'lavender'
+            }}>{props.priority} </p>
             <button onClick={props.markDone}className="doneButton">Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
 
